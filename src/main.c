b39b4a9f8af5a8228ab14ab2920a84adf000ab7d
Jakub Gonet <jakub.gonet0@gmail.com>
2018-02-14 18:05:41 +0100

    Add main loop logic

--- src/main.c
+++ src/main.c
@@ -1,44 +1,97 @@
 #include "../includes/settings.h"
 #include "../includes/main.h"
+#include "../includes/oneWire.h"
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
 void initPins(void);
-void init1Wire(void);
 void setNumberInNixie(uint8_t tubeNumber, uint8_t number);
 void presetNixieNumberFromLookup(uint8_t number);
 void enableNixie(uint8_t tubeNumber);
+uint8_t convertTemp(uint16_t temp);
 
 /*
  * Lookup table is used because pins ABCD of 74141 isn't connected to one port, but two instead (B, D).
- * Binary values are inversed, because A is MSB and D is LSB.
+ * Binary values are inversed because A is MSB and D is LSB.
  *
  * pattern: [DCBA]              0-0000 1-1000 2-0100 3-1100 4-0010 5-1010 6-0110 7-1110 8-0001 9-1001
  */
 uint8_t numbersLookupTable[] = {0b000, 0b100, 0b010, 0b110, 0b001, 0b101, 0b011, 0b111, 0b000, 0b100};
 
+uint8_t sign = 0;
+uint8_t temp = 0;
+
 int main(){
   init();
-  while(1){
+  int cycle = 0;
 
+  while(1){
+    _delay_ms(20);
+    cli();
+    if(!oneWire_ResetAndPresenceCheck()){
+      //DS18B20 not present, show -99
+      sign = 1;
+      temp = 0x99;
+    }
+    else{
+      uint8_t LSB = 0, MSB = 0;
+
+      switch(cycle){
+      case 0:
+        oneWire_sendByte(0xCC); //SKIP ROM
+        oneWire_sendByte(0x44); //CONVERT T
+        cycle++;
+        break;
+      case 1:
+        sei();
+        _delay_ms(750);
+        cli();
+        cycle++;
+        break;
+      case 3:
+        oneWire_ResetAndPresenceCheck();
+        cycle++;
+        break;
+      case 4:
+        oneWire_sendByte(0xCC); //SKIP ROM
+        oneWire_sendByte(0xBE); //READ SCRATCHPAD
+        cycle++;
+        break;
+      case 5:
+        LSB = oneWire_readByte();
+        MSB = oneWire_readByte();
+        cycle++;
+        break;
+      case 6:
+        oneWire_ResetAndPresenceCheck();
+        cycle++;
+        break;
+      case 7:
+        convertTemp((MSB<<8) | LSB);
+        cycle = 0;
+        break;
+      }
+    }
+    sei();
   }
   return 0;
 }
 
 void init(void){
   initPins();
-  init1Wire();
 }
 
 void initPins(void){
   //every pin as output
-  DDRB = 0xff;
-  DDRD = 0xff;
+  DDRB = 0xFF;
+  DDRD = 0xFF;
 }
 
-void init1Wire(void){
-
+uint8_t convertTemp(uint16_t temp){
+  if((temp>>11) & 0x1)
+    temp = ~temp + 1;
+  return (temp>>4) & 0x7F;
 }
 
 void presetNixieNumberFromLookup(uint8_t number){
